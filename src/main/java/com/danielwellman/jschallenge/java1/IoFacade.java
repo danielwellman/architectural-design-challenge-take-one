8e4beefac3ca801a10430e1b7a4bc80f84b14dfe
Daniel Wellman <etldan@gmail.com>
2011-08-10 17:39:42 -0400

    Adding a note

--- src/main/java/com/danielwellman/jschallenge/java1/IoFacade.java
+++ src/main/java/com/danielwellman/jschallenge/java1/IoFacade.java
@@ -1,5 +1,7 @@
 package com.danielwellman.jschallenge.java1;
 
+// This interface is weird; it's only required because
+// the Converter needs to provide dependencies that can
+// both read and write to the file system.
 public interface IoFacade extends IoWriter, IoReader {
-
 }
